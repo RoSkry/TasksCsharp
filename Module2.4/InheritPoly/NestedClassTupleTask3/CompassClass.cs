3d4f932be535c66ce2b714544b0f022c92819469
SkripnikRostislav <skrypnik-r@ukr.net>
2018-11-18 01:13:08 +0200

    Third finished

---
+++ Module2.4/InheritPoly/NestedClassTupleTask3/CompassClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NestedClassTupleTask3
+{
+    public class CompassClass
+    {
+        Random _random = new Random();
+        //Создать вложенный enum Compass со значениям сторон света(N, S, E, W)
+        public enum Compass
+        {
+            North,South,East,West
+        }
+       //Создать влаженною структуру GeoCoordinate(свойства Value типа double; Suffix типа Compass)
+        public struct GeoCoordinate
+        {
+            public double Value { get; set; }
+            public Compass Suffix { get; set; }
+        }
+        //Создать метод GetRandomCoordinates возвращаюший кортеж(tuple) типа(GeoCoordinate Latitude, GeoCoordinate Longtitude)
+//В этом методе создать локальную функцию генерирующую GeoCoordinate из случайных чисел(случайное число для value, и случайное для Compass) : на вход ограничение для координат(широта на больше 90 долгота не больше 180, стороны света должны соответствовать)
+        public (GeoCoordinate Latitude, GeoCoordinate Longtitude) GetRandomCoordinates()
+        {
+           // int s = _random.Next(1, 4);
+            GeoCoordinate lat = new GeoCoordinate { Suffix = (Compass)_random.Next(1, 4), Value = _random.Next(0, 90) };
+            GeoCoordinate lon =new GeoCoordinate { Suffix = (Compass)_random.Next(1, 4), Value = _random.Next(0, 180) };
+            return (lat, lon);
+        }
+    }
+}
---
+++ Module2.4/InheritPoly/NestedClassTupleTask3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace NestedClassTupleTask3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 3.Работа с nested class & tuples
+            //Создать вложенный enum Compass со значениям сторон света(N, S, E, W)           
+//Создать влаженною структуру GeoCoordinate(свойства Value типа double; Suffix типа Compass)
+//Создать метод GetRandomCoordinates возвращаюший кортеж(tuple) типа(GeoCoordinate Latitude, GeoCoordinate Longtitude)
+//В этом методе создать локальную функцию генерирующую GeoCoordinate из случайных чисел(случайное число для value, и случайное для Compass) : на вход ограничение для координат(широта на больше 90 долгота не больше 180, стороны света должны соответствовать)
+//Из метода мейн в переменную вернуть значение GetRandomCoordinates; Переменную объявит неявно через var.Вывести на экран что получилось
+             CompassClass compassClass = new CompassClass();
+            var coordinates = compassClass.GetRandomCoordinates();
+            Console.WriteLine($"Latitude {coordinates.Latitude.Value}, Longtitude {coordinates.Longtitude.Value}");
+
+
+    }
+    }
+}
