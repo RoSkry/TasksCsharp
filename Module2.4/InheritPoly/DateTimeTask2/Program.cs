4e4f43dc85c4eb30194e9c4b31811038cd119ff8
SkripnikRostislav <skrypnik-r@ukr.net>
2018-11-17 18:57:08 +0200

    Added second task

---
+++ Module2.4/InheritPoly/DateTimeTask2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DateTimeTask2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+        }
+    }
+}
--- Module2.4/InheritPoly/GuidTask1/Program.cs
+++ Module2.4/InheritPoly/GuidTask1/Program.cs
@@ -6,7 +6,28 @@ namespace GuidTask1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //            1.Работа с Guid
+            //Сгенерировать пустой гуид
+            Guid empty = Guid.Empty;
+            //Сгенерировать новый гуид
+            Guid guid = Guid.NewGuid();
+            //Привести гуид к строке и записать в переменную типа стринг
+            string guidString=Guid.NewGuid().ToString();
+            Console.WriteLine(guidString);
+            //Изменить строку по свою усмотрению(помнить о формате гуида - 32 символа 0 - F  с дефисами)
+            guidString = "F9168C5E-CEB2-4faa-B6BF-329BF39FA1E4";
+            //СОздать переменную типа гуид, распарсить строку полученную ранее и записать в созданную переменную.
+            Guid parse = Guid.Parse(guidString);
+            //Повторить предыдущий пункт через try parse, притом инициализировать переменную в аргументе метода try parse как out параметр, а сам метод Guid.TryParse поместить в if как условие.Если условие выполнено и строка распаршено успешно - вывести гуид на экран с сообщение Parsed successfully, если нет -вывести сообщение "String is not a valid guid"           
+            if(Guid.TryParse(guidString,out Guid parse1))
+            {
+                Console.WriteLine("Parsed successfully");
+            }
+            else
+            {
+                Console.WriteLine("String is not a valid guid");
+            }
+
         }
     }
 }
