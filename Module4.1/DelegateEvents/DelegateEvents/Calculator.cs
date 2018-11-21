a27d3811cf0ad71c1d16a06d3bb19647a126266c
SkripnikRostislav <skrypnik-r@ukr.net>
2018-11-21 12:27:26 +0200

    Added first delegate task

---
+++ Module4.1/DelegateEvents/DelegateEvents/Calculator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DelegateTask1
+{
+    ////Создать класс калькулятор с методами DoSum, DoSubstraction, DoDivision, DoMultiplication.Каждый из них принимает два инта и возвращает дабл.
+    public class Calculator
+    {
+        public double DoSum(int x, int y)
+        {
+            Console.WriteLine($"Sum: {x + y}");
+            return x + y;
+        }
+        public double DoSubstraction(int x, int y)
+        {
+            Console.WriteLine($"Substraction: {x - y}");
+            return x - y;
+        }
+        public double DoMultiplication(int x, int y)
+        {
+            Console.WriteLine($"Multiplication: {x * y}");
+            return x * y;
+        }
+        public double DoDivision(int x, int y)
+        {
+            if(y!=0)
+            {
+                Console.WriteLine($"Division: {x / y}");
+                return x / y;
+            }
+            throw new DivideByZeroException();
+        }
+    }
+}
--- Module4.1/DelegateEvents/DelegateEvents/Program.cs
+++ Module4.1/DelegateEvents/DelegateEvents/Program.cs
@@ -1,12 +1,45 @@
-﻿using System;
+﻿
+using System;
 
-namespace DelegateEvents
+namespace DelegateTask1
 {
+    public delegate double Calculate(int x, int y);
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //            Кейс по делегатам
+            //Создать класс калькулятор с методами DoSum, DoSubstraction, DoDivision, DoMultiplication.Каждый из них принимает два инта и возвращает дабл.
+            //Создать делегат примающий два инта и возвращающий дабл.
+            //Создать инстанс делегата и записать в его invocation list указанные четыре метода калькулятора.
+            //Вызвать методы из инвокейшн листа делегата через его метод Invoke(передать в Invoke пару переменных типа инт)
+            try
+            {
+                Calculator calculator = new Calculator();
+                Calculate calculate = calculator.DoSum;
+                calculate += calculator.DoSubstraction;
+                calculate += calculator.DoMultiplication;
+                calculate += calculator.DoDivision;
+              
+                calculate.Invoke(12, 0);
+                
+                
+             
+            }
+            catch (DivideByZeroException de)
+            {
+                Console.WriteLine(de.Message);
+            }
+            catch (ArithmeticException ar)
+            {
+                Console.WriteLine(ar.Message);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+
+            }
         }
     }
 }
